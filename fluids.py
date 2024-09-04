da129c851cd1378a331fb2ceef7f23ba7c5758d4
Jamil Rahman <fx78901@gmail.com>
2024-09-04 13:45:22 +0100

    animated simple diffusion physics beginning from any point in the grid

--- fluids.py
+++ fluids.py
@@ -2,24 +2,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-grid_size = (100, 100)
+grid_size = (11, 11)
 grid = np.zeros(grid_size)
-grid[41:60, 41:60] = 200
+grid[5, 5] = 500
 
 fig = plt.figure(facecolor="b")
-ax = plt.axes(xlim=(0, 100), ylim=(0, 100))
+ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
 
 im = ax.imshow(grid, cmap='hot', interpolation='none', vmin=0, vmax=255)
 ax.grid(color='white', linestyle='-', linewidth=0.5)
 
+previous_grid = np.copy(grid)
+
+
+def init():
+    global previous_grid
+    previous_grid = np.copy(grid)
+    im.set_array(grid)
+    return [im]
+
+
+def diffusion(central, left, right, top, down):
+    diffusion_factor = 0.1
+    return central + diffusion_factor * (left + right + top + down - 4 * central)
+
 
 def update_grid(frame):
-    print(frame)
-    grid[40:61, 40:61] = 255 - frame
+    global previous_grid
+    new_grid = np.copy(previous_grid)
+
+    for i in range(1, grid_size[0] - 1):
+        for j in range(1, grid_size[1] - 1):
+
+            central = previous_grid[i, j]
+            left = previous_grid[i, j - 1]
+            right = previous_grid[i, j + 1]
+            top = previous_grid[i - 1, j]
+            down = previous_grid[i + 1, j]
+
+            new_grid[i, j] = diffusion(central, left, right, top, down)
+
+    grid[:] = new_grid
+    previous_grid = np.copy(grid)
 
     im.set_array(grid)
     return [im]
 
-animation = FuncAnimation(fig, update_grid, frames=256, interval=25)
+
+animation = FuncAnimation(fig, update_grid, init_func=init, frames=1000, interval=25)
 
 plt.show()
